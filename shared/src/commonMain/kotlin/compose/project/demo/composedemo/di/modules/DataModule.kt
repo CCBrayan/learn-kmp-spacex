b9f965df905a2ff64e58782c8d70a5f3b792036e
Brayanjax <brajo2604@gmail.com>
2026-02-24 19:36:20 -0600

    Parte4

--- shared/src/commonMain/kotlin/compose/project/demo/composedemo/di/modules/DataModule.kt
+++ shared/src/commonMain/kotlin/compose/project/demo/composedemo/di/modules/DataModule.kt
@@ -1,12 +1,17 @@
 package compose.project.demo.composedemo.di.modules
 
+import compose.project.demo.composedemo.data.local.AppDatabase
+import compose.project.demo.composedemo.data.local.AppDatabaseQueries
+import compose.project.demo.composedemo.data.local.DriverFactory
+import compose.project.demo.composedemo.data.local.ILocalRocketLaunchesDataSource
+import compose.project.demo.composedemo.data.local.LocalRocketLaunchesDataSource
 import compose.project.demo.composedemo.data.remote.IRemoteRocketLaunchesDataSource
 import compose.project.demo.composedemo.data.remote.RemoteRocketLaunchesDataSource
+import compose.project.demo.composedemo.data.repository.IRocketLaunchesRepository
+import compose.project.demo.composedemo.data.repository.RocketLaunchesRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import org.koin.dsl.module
-
-
 
 val dataModule = module {
     single<IRemoteRocketLaunchesDataSource> {
@@ -15,4 +20,21 @@ val dataModule = module {
             Dispatchers.IO
         )
     }
+<<<<<<< HEAD
+    single<IRocketLaunchesRepository>
+    { RocketLaunchesRepository(get(),
+        get(),
+        Dispatchers.Default)
+    }
 }
+=======
+
+    single { get<DriverFactory>().createDriver() }
+    single { AppDatabase(get()) }
+    
+    // Provee explícitamente las queries para que LocalRocketLaunchesDataSource las reciba
+    single<AppDatabaseQueries> { get<AppDatabase>().appDatabaseQueries }
+    
+    single<ILocalRocketLaunchesDataSource> { LocalRocketLaunchesDataSource(get()) }
+}
+>>>>>>> a3fabf325d0aecdc649d2bae20573ea98bc722e8
